c0c2d06e8f21b6c7880a881979291b3a0c4901b1
Taras Kunch <taras.kunch@gmail.com>
2015-05-20 20:15:22 +0300

    open-uri

--- antons_homework/06.05/open_uri/open_uri.rb
+++ antons_homework/06.05/open_uri/open_uri.rb
@@ -1,14 +1,16 @@
+require "openssl"
 require "open-uri"
 require "sanitize"
 
 uri = URI.parse("https://www.ruby-lang.org/en/")
 source = ""
-uri.open {|f|
+open('https://www.ruby-lang.org/en/', {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}) {|f|
   f.each_line do |line|
-  	if line.match(/<span class="[[:alnum:]][[:alnum:]]">/)
-  		source += line
-  	end
+    if line.match(/<span class="[[:alnum:]][[:alnum:]]">/)
+      source += line
+    end
   end
   source = Sanitize.fragment(source)
-  File.write('parseSource.txt', source)
+ # File.write('parseSource.txt', source)
+  puts source
 }
